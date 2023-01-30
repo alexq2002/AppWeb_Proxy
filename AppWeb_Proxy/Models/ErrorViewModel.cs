514af2282c175615de656c3dd3e9d1de50d944dc
alexq2002 <115095396+alexq2002@users.noreply.github.com>
2023-01-29 23:17:44 -0500

    Agregar archivos de proyecto.

---
+++ AppWeb_Proxy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppWeb_Proxy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
